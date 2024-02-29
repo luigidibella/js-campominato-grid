1779d7e4b78c6a043df19ab3491c33b6d53aaa69
luigidibella <lykos.omega@gmail.com>
2024-02-29 18:58:00 +0100

    [add] debug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
-const playButton = document.querySelector('.play')
+const playButton = document.querySelector('.play');
+
 
 const totalCells = 100;
 /* const totalCells = 81; */
@@ -8,14 +9,28 @@ const totalCells = 100;
 
 playButton.addEventListener('click', play);
 
-
 function play(){
+  grid.innerHTML = '';
   for(let i = 1; i <= totalCells; i++){
-    console.log(i);
+    /* console.log(i); */
     const cell = document.createElement('div');
-    cell.innerHTML += `<div class="num">${[i]}</li>`
+    cell.innerHTML += `<div class="num">${[i]}</div>`;
     cell.classList.add('cell');
-  
+    cell.classList.add('check');
+    
     grid.appendChild(cell);
   }
+}
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', check);
+
+// Seleziona l'elemento con la classe "num"
+let numElement = document.querySelector('.num');
+
+// Leggi il contenuto testuale dell'elemento
+let num = numElement.textContent;
+
+function check(){
+  console.log(num);
 }
